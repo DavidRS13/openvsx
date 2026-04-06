241a92d72ea79bdddc3d9969b8b8b95ed78449fc
DavidRS13 <david@codetogether.com>
2026-04-06 12:09:51 -0600

    feat: add RootGreeting class and update RootHello with additional output and test method

---
+++ RootGreeting.java
@@ -0,0 +1,7 @@
+public class RootGreeting {
+    public static void main(String[] args) {
+        System.out.println("Hello from another root Java file");
+        System.out.println("RootGreeting is ready to run");
+    }
+}
+
--- RootHello.java
+++ RootHello.java
@@ -2,6 +2,18 @@ public class RootHello {
     public static void main(String[] args) {
         System.out.println("Hello from openvsx root Java file");
         System.out.println("This is me");
+        System.out.println("This is me again lol");
+
+        public void myTest()
+        {
+            // This is going to be a big test
+            int a=3;
+            int b=4;
+
+            int sum = a+b;
+
+            System.out.println("Sum: " + sum.toString);
+        }
     }
 }
 
